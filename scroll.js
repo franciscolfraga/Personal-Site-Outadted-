8a8523beb9d1fb1b95f86f385131a9f86396d9f1
franciscolfraga <franciscolfraga@gmail.com>
2017-08-23 17:28:34 +0100

    warming up!

--- scroll.js
+++ scroll.js
@@ -6,94 +6,84 @@ $(window).scroll(function() {
         $(".balls").removeClass("scrollabout");
     }
     if (scroll >= 500) {
+        $(".about").addClass("style11b");
+    } else {
+        $(".about").removeClass("style11b");
+    }
+    if (scroll >= 500) {
         $(".about").addClass("scrollopacity");
     } else {
         $(".about").removeClass("scrollopacity");
     }
-    if (scroll >= 1050) {
+    if (scroll >= 1100) {
         $(".about").addClass("scroll1");
     } else {
         $(".about").removeClass("scroll1");
     }
-    if (scroll >= 1100) {
+    if (scroll >= 1175) {
         $(".about").addClass("scroll2");
     } else {
         $(".about").removeClass("scroll2");
     }
-    if (scroll >= 1150) {
+    if (scroll >= 1250) {
         $(".about").addClass("scroll3");
     } else {
         $(".about").removeClass("scroll3");
     }
-    if (scroll >= 1420) {
+    if (scroll >= 1620) {
         $(".about").addClass("scroll-achiv1");
     } else {
         $(".about").removeClass("scroll-achiv1");
     }
-    if (scroll >= 1470) {
+    if (scroll >= 1670) {
         $(".about").addClass("scroll-achiv2");
     } else {
         $(".about").removeClass("scroll-achiv2");
     }
-    if (scroll >= 1520) {
+    if (scroll >= 1720) {
         $(".about").addClass("scroll-achiv3");
     } else {
         $(".about").removeClass("scroll-achiv3");
     }
-    if (scroll >= 1570) {
+    if (scroll >= 1770) {
         $(".about").addClass("scroll-achiv4");
     } else {
         $(".about").removeClass("scroll-achiv4");
     }
-    if (scroll >= 1850) {
+    if (scroll >= 2050) {
         $(".about").addClass("scroll-edu1");
     } else {
         $(".about").removeClass("scroll-edu1");
     }
-    if (scroll >= 1900) {
+    if (scroll >= 2100) {
         $(".about").addClass("scroll-edu2");
     } else {
         $(".about").removeClass("scroll-edu2");
     }
-    if (scroll >= 1950) {
+    if (scroll >= 2150) {
         $(".about").addClass("scroll-edu3");
     } else {
         $(".about").removeClass("scroll-edu3");
     }
-    if (scroll >= 2400) {
+    if (scroll >= 2500) {
         $(".about").addClass("scroll-skill1");
     } else {
         $(".about").removeClass("scroll-skill1");
     }
-    if (scroll >= 2450) {
+    if (scroll >= 2575) {
         $(".about").addClass("scroll-skill2");
     } else {
         $(".about").removeClass("scroll-skill2");
     }
-    if (scroll >= 2500) {
+    if (scroll >= 2650) {
         $(".about").addClass("scroll-skill3");
     } else {
         $(".about").removeClass("scroll-skill3");
     }
-    if (scroll >= 2550) {
+    if (scroll >= 2725) {
         $(".about").addClass("scroll-skill4");
     } else {
         $(".about").removeClass("scroll-skill4");
-    }
-    if (scroll >= 2600) {
-        $(".about").addClass("scroll-skill5");
-    } else {
-        $(".about").removeClass("scroll-skill5");
-    }
-    if (scroll >= 2650) {
-        $(".about").addClass("scroll-skill6");
-    } else {
-        $(".about").removeClass("scroll-skill6");
-    }
-    if (scroll >= 2700) {
-        $(".about").addClass("scroll-skill7");
-    } else {
-        $(".about").removeClass("scroll-skill7");
     }
     if (scroll >= 0 && scroll < 700) {
         $(".balls").addClass("scrollcover");
