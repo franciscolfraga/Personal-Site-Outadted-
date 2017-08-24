a6a753bf2d0861304fc50fc1211738c67b01f870
franciscolfraga <franciscolfraga@gmail.com>
2017-08-24 17:10:17 +0100

    nothing important

--- scroll.js
+++ scroll.js
@@ -15,72 +15,72 @@ $(window).scroll(function() {
     } else {
         $(".about").removeClass("scrollopacity");
     }
-    if (scroll >= 1100) {
+    if (scroll >= 1000) {
         $(".about").addClass("scroll1");
     } else {
         $(".about").removeClass("scroll1");
     }
-    if (scroll >= 1175) {
+    if (scroll >= 1075) {
         $(".about").addClass("scroll2");
     } else {
         $(".about").removeClass("scroll2");
     }
-    if (scroll >= 1250) {
+    if (scroll >= 1150) {
         $(".about").addClass("scroll3");
     } else {
         $(".about").removeClass("scroll3");
     }
-    if (scroll >= 1620) {
+    if (scroll >= 1520) {
         $(".about").addClass("scroll-achiv1");
     } else {
         $(".about").removeClass("scroll-achiv1");
     }
-    if (scroll >= 1670) {
+    if (scroll >= 1570) {
         $(".about").addClass("scroll-achiv2");
     } else {
         $(".about").removeClass("scroll-achiv2");
     }
-    if (scroll >= 1720) {
+    if (scroll >= 1620) {
         $(".about").addClass("scroll-achiv3");
     } else {
         $(".about").removeClass("scroll-achiv3");
     }
-    if (scroll >= 1770) {
+    if (scroll >= 1670) {
         $(".about").addClass("scroll-achiv4");
     } else {
         $(".about").removeClass("scroll-achiv4");
     }
-    if (scroll >= 2050) {
+    if (scroll >= 1950) {
         $(".about").addClass("scroll-edu1");
     } else {
         $(".about").removeClass("scroll-edu1");
     }
-    if (scroll >= 2100) {
+    if (scroll >= 2000) {
         $(".about").addClass("scroll-edu2");
     } else {
         $(".about").removeClass("scroll-edu2");
     }
-    if (scroll >= 2150) {
+    if (scroll >= 2050) {
         $(".about").addClass("scroll-edu3");
     } else {
         $(".about").removeClass("scroll-edu3");
     }
-    if (scroll >= 2500) {
+    if (scroll >= 2400) {
         $(".about").addClass("scroll-skill1");
     } else {
         $(".about").removeClass("scroll-skill1");
     }
-    if (scroll >= 2575) {
+    if (scroll >= 2475) {
         $(".about").addClass("scroll-skill2");
     } else {
         $(".about").removeClass("scroll-skill2");
     }
-    if (scroll >= 2650) {
+    if (scroll >= 2550) {
         $(".about").addClass("scroll-skill3");
     } else {
         $(".about").removeClass("scroll-skill3");
     }
-    if (scroll >= 2725) {
+    if (scroll >= 2625) {
         $(".about").addClass("scroll-skill4");
     } else {
         $(".about").removeClass("scroll-skill4");
@@ -93,7 +93,6 @@ $(window).scroll(function() {
 });
 
 
-//from https://css-tricks.com/snippets/jquery/smooth-scrolling/
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -108,3 +107,30 @@ $(function() {
     }
   });
 });
+
+/*window.setInterval(function(){
+
+  var bool = Math.floor(Math.random() * 3) + 0;
+  console.log(bool);
+
+  if(bool==0){
+    $("#name:after").css("transform-origin", " 0%");
+    $("#cover").addClass("try");
+    setTimeout(function(){
+      $("#cover").removeClass("try");
+    }, 2000);
+  }
+  else if(bool==1){
+    setTimeout(function(){
+    }, 2000);
+  }
+  else if(bool==2){
+    $("#name:after").css("transform-origin", " 100% 50%");
+    $("#cover").addClass("try");
+    setTimeout(function(){
+      $("#name:after").css("transform-origin", " 50% 100%");
+      $("#cover").removeClass("try");
+    }, 2000);
+  }
+
+}, 3000);*/
